4a1c3979d07b1122bf8655f342b6c25e5dd09f54
SHalima8 <shalima.bsai24seecs@seecs.edu.pk>
2026-07-09 15:23:06 +0500

    auto commit: 2026-07-09 15:23:06

--- phase-3-rag-systems/day-17/scratch.py
+++ phase-3-rag-systems/day-17/scratch.py
@@ -1,4 +1,4 @@
 from vector_stores.store_utils import chroma_list_collections, faiss_list_chunk_ids
 
 print(chroma_list_collections(store))       # if using your chroma test store variable
-print(faiss_list_chunk_ids(faiss_store))    # if using your faiss test store variablewhe
+print(faiss_list_chunk_ids(faiss_store))    # if using your faiss test store variable
